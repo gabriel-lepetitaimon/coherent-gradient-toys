1d0a336edf70d5b47cdc2baccba04f6af707304d
Gabriel LEPETIT-AIMON <gabriel.lepetit-aimon@polymtl.ca>
2021-07-18 23:32:47 -0400

    Fix Bezier Curves

--- lines.py
+++ lines.py
@@ -141,11 +141,9 @@ class Line:
     A = PointAttr()
     B = PointAttr()
 
-    def __init__(self, a, b, profile, width):
+    def __init__(self, a, b):
         self.A = a
         self.B = b
-        self.profile = profile
-        self.width = width
 
     def __repr__(self):
         return f'Line: {self.A} - {self.B}'
@@ -179,13 +177,22 @@ class Line:
             d[halfplanes] = np.nan
         return d
 
-    def draw_line(self, h, w, subsample=1, roundtip=False, device=None):
-        d = self._distance_field(h, w, subsample, roundtip, device=device) / self.width
-        p = self.profile(d)
+    def draw_line(self, h, w, subsample=1, width=3, profile=None, roundtip=False, device=None):
+        d = self._distance_field(h, w, subsample, roundtip, device=device)
+        return Line.dist_to_line(d, subsample=subsample, width=width, profile=profile)
+
+    @staticmethod
+    def dist_to_line(d, subsample=1, width=3, profile=None):
+        d = d/width
+        if profile is None:
+            p = torch.abs(d) <= .5
+        else:
+            p = profile(d)
         p[p.isnan()] = 0
         if subsample != 1:
             from torch.nn.functional import avg_pool2d
-            p = avg_pool2d(p[None], subsample)[0]
+            p = avg_pool2d(p[None].float(), subsample)[0]
+
         return p
 
     def draw_squeleton(self, h, w, device=None):
@@ -218,64 +225,15 @@ class Line:
 class QuadBezierLine(Line):
     C = PointAttr()
 
-    def __init__(self, a, b, c, profile, width):
-        super(QuadBezierLine, self).__init__(a, b, profile, width)
+    def __init__(self, a, b, c):
+        super(QuadBezierLine, self).__init__(a, b)
         self.C = c
 
-    def _distance_field(self, h, w, scale=1, roundtip=False):
-        a = self.A
-        b = self.B
-        u = (b-a).unitary()
+    def _distance_field(self, h, w, scale=1, roundtip=False, device=None):
         offset = (1-1/scale)/2
-        x, y = torch.meshgrid(torch.arange(-offset, h-offset, 1/scale),
-                              torch.arange(-offset, w-offset, 1/scale))
-        d = u.x*(y-a.y) - u.y*(x-a.x)
-
-        a_halfplane = (x-a.x)*u.x + (y-a.y)*u.y < 0
-        b_halfplane = (x-b.x)*u.x + (y-b.y)*u.y > 0
-
-        if roundtip:
-            if not isinstance(roundtip, str):
-                d[a_halfplane] = torch.sqrt(torch.square(x[a_halfplane]-a.x)+torch.square(y[a_halfplane]-a.y))
-                d[b_halfplane] = torch.sqrt(torch.square(x[b_halfplane]-b.x)+torch.square(y[b_halfplane]-b.y))
-            elif roundtip.lower() == 'a':
-                d[a_halfplane] = torch.sqrt(torch.square(x[a_halfplane]-a.x)+torch.square(y[a_halfplane]-a.y))
-                d[b_halfplane] = np.nan
-            elif roundtip.lower() == 'b':
-                d[a_halfplane] = np.nan
-                d[b_halfplane] = torch.sqrt(torch.square(x[b_halfplane]-b.x)+torch.square(y[b_halfplane]-b.y))
-            else:
-                roundtip = False
-        if not roundtip:
-            halfplanes = a_halfplane | b_halfplane
-            d[halfplanes] = np.nan
-        return d
-
-    def draw_squeleton(self, h, w):
-        a, b = self.A, self.B
-
-        if a.x == b.x:
-            x = math.floor(a.x)
-            y1 = math.floor(min(a.y, b.y))
-            y2 = math.ceil(max(a.y, b.y))
-            skeleton = torch.zeros((h,w), dtype=torch.bool)
-            skeleton[x, y1:y2+1] = True
-            return skeleton
-        else:
-            slope = (a.y-b.y) / (a.x-b.x)
-            offset = (b.y*a.x - a.y*b.x) / (a.x-b.x)
-            u = (b-a).unitary()
-            x, y = torch.meshgrid(torch.arange(0, h), torch.arange(0, w))
-
-            y1 = slope*x+offset
-            y2 = slope*(x+1)+offset
-            skeleton = (((y <= y1) & (y1 < y+1)) | ((y <= y2) & (y2 < y+1)))
-            if slope != 0:
-                x1 = (y-offset)/slope
-                skeleton |= (x <= x1) & (x1 < x+1)
-
-            domain = ((x-a.x)*u.x + (y-a.y)*u.y >= 0) & ((x-b.x)*u.x + (y-b.y)*u.y <= 0)
-            return skeleton & domain
+        x, y = torch.meshgrid(torch.arange(-offset, h-offset, 1/scale, device=device),
+                              torch.arange(-offset, w-offset, 1/scale, device=device))
+        return bezierDistance(self.A, self.C, self.B, x, y)
 
 
 ##################################################
@@ -284,68 +242,86 @@ class QuadBezierLine(Line):
 
 # Inspired by shadertoy.com/view/ltXSDB
 
-# # Test if point p crosses line (a, b), returns sign of result
-# def testCross(a, b, px, py):
-#     return torch.sign((b.y-a.y) * (px-a.x) - (b.x-a.x) * (py-a.y))
-#
-#
-# def mix(x, y, a):
-#     return x*a+y*(1-a)
-#
-# # Determine which side we're on (using barycentric parameterization)
-# def signBezier( A,  B,  C,  px, py):
-#     a = C - A
-#     b = B - A
-#     cx, cy= px - A.x, py - A.y
-#     bary = cx*b.y-b.x*cy , a.x*cy-cx*a.y
-#     bary = [_ / (a.x*b.y-b.x*a.y) for _ in bary]
-#     baryx, baryy = bary
-#     d =  1.0 - baryx - baryy;
-#     dx, dy = baryy * 0.5 + d, d
-#     return mix(torch.sign(dx * dx - dy), mix(-1.0, 1.0,
-#                         testCross(A, B, px, py) * testCross(B, C, px, py) > 0.0),
-#                     ((d.x - d.y) > 0.0) * testCross(A, C, B.x, B.y))
-#
-#
+# Test if point p crosses line (a, b), returns sign of result
+def testCross(a, b, px, py):
+    c = (b.y-a.y) * (px-a.x) - (b.x-a.x) * (py-a.y)
+    if not isinstance(c, torch.Tensor):
+        c = torch.Tensor([c])
+    return torch.sign(c)
+
+
+def mix(x, y, a):
+    return x*(1-1*a)+y*(1*a)
+
+
+def vec_norm(x, y):
+    return torch.norm(torch.stack([x, y]), dim=0)
+
+
+# Determine which side we're on (using barycentric parameterization)
+def signBezier( A,  B,  C,  px, py):
+    a = C - A
+    b = B - A
+    cx, cy = px - A.x, py - A.y
+    bary = cx*b.y-b.x*cy, a.x*cy-cx*a.y
+    bary = [_ / (a.x*b.y-b.x*a.y) for _ in bary]
+    baryx, baryy = bary
+    d = 1.0 - baryx - baryy
+    dx, dy = baryy * 0.5 + d, d
+
+    return mix(torch.sign(dx * dx - dy),
+               mix(-1.0, 1.0, (testCross(A, B, px, py) * testCross(B, C, px, py)) >= 0.0),
+               ((dx - dy) >= 0.0)) * testCross(A, C, B.x, B.y)
+
+
 # # Solve cubic equation for roots
-# def solveCubic(a, b, c):
-#     p = b - a*a / 3.0, p3 = p*p*p;
-#     q = a * (2.0*a*a - 9.0*b) / 27.0 + c;
-#     float d = q*q + 4.0*p3 / 27.0;
-#     float offset = -a / 3.0;
-#     if(d >= 0.0):
-#         float z = sqrt(d);
-#         vec2 x = (vec2(z, -z) - q) / 2.0;
-#         vec2 uv = sign(x)*pow(abs(x), vec2(1.0/3.0));
-#         return vec3(offset + uv.x + uv.y);
-#
-#     float v = acos(-sqrt(-27.0 / p3) * q / 2.0) / 3.0;
-#     float m = cos(v), n = sin(v)*1.732050808;
-#     return vec3(m + m, -n - m, n - m) * sqrt(-p / 3.0) + offset;
-#
-#
-# # Find the signed distance from a point to a bezier curve
-# def sdBezier(A, B, C, px, py):
-#     Bx = B.x if B.x * 2.0 - A.x - C.x != 0 else B.x + 1e-4
-#     By = B.y if B.y * 2.0 - A.x - C.x != 0 else B.y + 1e-4
-#     B = Point(Bx, By)
-#
-#     a = B - A
-#     b = A - B * 2.0 + C
-#     c = a * 2.0
-#     dx, dy = A.x - px, A.y - py
-#
-#     # vec3 k = vec3(3.*dot(a,b), 2.*dot(a,a)+dot(d,b), dot(d,a)) / dot(b,b)
-#     # vec3 t = clamp(solveCubic(k.x, k.y, k.z), 0.0, 1.0)
-#     tx, ty = solveCubic(3*np.dot(a,b),
-#                         2*np.dot(a,a) + dx*b.x+dy*b.y,
-#                         (dx*a.x+dy*a.y) / np.dot(b,b))
-#     tx, ty = [torch.clip(_, 0, 1) for _ in (tx, ty)]
-#
-#     pos = A + (c + b*tx)*tx
-#     float dis = torch.sqrt(pos - p)
-#     pos = A + (c + b*t.y)*t.y
-#     dis = min(dis, length(pos - p))
-#     pos = A + (c + b*t.z)*t.z
-#     dis = min(dis, length(pos - p))
-#     return dis * signBezier(A, B, C, p)
+def solveCubic(a, b, c):
+    p = b - a*a / 3.0
+    p3 = p*p*p
+    q = a * (2.0*a*a - 9.0*b) / 27.0 + c
+    d = q*q + 4.0*p3 / 27.0
+    offset = -a / 3.0
+
+    z = torch.sqrt(torch.abs(d))
+    x = (torch.stack([z, -z]) - q) / 2.0
+    uv = torch.sign(x)*torch.pow(torch.abs(x), 1/3)
+    r = offset + uv[0] + uv[1]
+
+    v = torch.acos(-torch.sqrt(-27.0 / p3) * q / 2.0) / 3.0
+    m = torch.cos(v)
+    n = torch.sin(v)*1.732050808
+
+    return tuple(torch.where(d >= 0, r, (_ * torch.sqrt(-p / 3.0) + offset))
+                 for _ in [m + m, -n - m, n - m])
+
+
+# Find the signed distance from a point to a bezier curve
+def bezierDistance(A, B, C, px, py):
+    Bx = B.x if B.x * 2.0 - A.x - C.x != 0 else B.x + 1e-4
+    By = B.y if B.y * 2.0 - A.x - C.x != 0 else B.y + 1e-4
+    B = Point(Bx, By)
+
+    a = B - A
+    b = A - B * 2.0 + C
+    c = a * 2.0
+    dx, dy = A.x - px, A.y - py
+
+    # vec3 k = vec3(3.*dot(a,b), 2.*dot(a,a)+dot(d,b), dot(d,a)) / dot(b,b)
+    # vec3 t = clamp(solveCubic(k.x, k.y, k.z), 0.0, 1.0)
+    k = 3*np.dot(a, b), 2*np.dot(a, a) + dx*b.x+dy*b.y, dx*a.x+dy*a.y
+    t1, t2, t3 = solveCubic(*[_ / np.dot(b,b) for _ in k])
+    t1, t2, t3 = [torch.clip(_, 0, 1) for _ in (t1, t2, t3)]
+
+    posX = A.x + (c.x + b.x*t1)*t1
+    posY = A.y + (c.y + b.y*t1)*t1
+    dis = vec_norm(posX - px, posY-py)
+
+    posX = A.x + (c.x + b.x*t2)*t2
+    posY = A.y + (c.y + b.y*t2)*t2
+    dis = torch.min(dis, vec_norm(posX - px, posY-py))
+
+    posX = A.x + (c.x + b.x*t3)*t3
+    posY = A.y + (c.y + b.y*t3)*t3
+    dis = torch.min(dis, vec_norm(posX - px, posY-py))
+
+    return dis * signBezier(A, B, C, px, py)
--- sample_generator.py
+++ sample_generator.py
@@ -11,20 +11,18 @@ class SampleGenerator:
         self.orientation = orientation
         self.length = length
 
-    def generate(self, b=8, h=512, device='cuda'):
+    def generate(self, b=8, h=512, y_width=3, device='cuda'):
         nlines = self.nlines() if callable(self.nlines) else self.nlines
         lines = []
         for i in range(b):
             blines = []
             for j in range(nlines):
-                width = self.width() if callable(self.width) else self.width
                 orientation = self.orientation() if callable(self.orientation) else self.orientation
                 length = self.length() if callable(self.length) else self.length
-                profile = self.profile()
                 dx, dy = np.cos(orientation)*length/2, np.sin(orientation)*length/2
                 x = np.random.rand()*(h-np.abs(dx)*2)+np.abs(dx)
                 y = np.random.rand()*(h-np.abs(dx)*2)+np.abs(dy)
-                blines += [Line(a=(x+dx, y+dy), b=(x-dx, y-dy), profile=profile, width=width)]
+                blines += [Line(a=(x+dx, y+dy), b=(x-dx, y-dy))]
             lines += [blines]
 
         x = torch.zeros(b, 1, h, h, device=device)
@@ -32,7 +30,9 @@ class SampleGenerator:
 
         for b, blines in enumerate(lines):
             for line in blines:
-                x[b, 0] += line.draw_line(h, h, device=device)
-                y[b] |= line.draw_squeleton(h, h, device=device)
+                profile = self.profile()
+                width = self.width() if callable(self.width) else self.width
+                x[b, 0] += line.draw_line(h, h, profile=profile, width=width, device=device, subsample=4)
+                y[b] |= line.draw_line(h, h, device=device, width=y_width)
 
         return x, y, lines
